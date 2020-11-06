504811b966bcd15144c4e361ac3d2b6c1f71c201
Miccs <35995570+Miccs@users.noreply.github.com>
2020-11-06 12:25:08 +0100

    Added colors page and color api

--- app.js
+++ app.js
@@ -71,6 +71,38 @@ getData = () => {
 app.get('/abc',(req,res)=>{ getData().then((results)=>{let html = '';for(let x in results)html+= results[x].kapitein+"<br>"; res.end(html)})});
 
 
+app.get('/colorpage/', (req, res) => {
+    res.sendFile(__dirname + "/Colors.html");
+});
+app.get('/colors/:name?', (req, res) => {
+    let sql = 'SELECT * FROM colors'
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (req.params.name)
+        {
+            var filteredres = new Array();
+            result.forEach(color => {
+                colorname = color.name.toLowerCase();
+                if (colorname.includes(req.params.name))
+                {
+                    filteredres.push(color);
+                }
+            });
+            res.send(filteredres);
+        }
+        else res.send(result);
+    });
+});
+
+app.get('/createcolorstable', (req, res) => {
+    let sql = 'CREATE TABLE colors(name VARCHAR(50), rgb VARCHAR(50), hsv VARCHAR(50), hex VARCHAR(12))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Color table created...');
+    });
+});
+
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
