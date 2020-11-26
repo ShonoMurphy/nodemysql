c4330b64776d4afba76559a8cd5fd3d81aadcbc4
Miccs <35995570+Miccs@users.noreply.github.com>
2020-11-26 11:41:55 +0100

    added account features

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const WebSocket = require('ws');
+var cookieParser = require('cookie-parser')
 
 // Create connection
 const db = mysql.createConnection({
@@ -23,13 +24,18 @@ process.on('uncaughtException', err => {
     process.exit(1) //mandatory (as per the Node.js docs)
 });
 
-function catchException(error, req, res)
+function catchException(error, req = null, res = null)
 {
     console.error('error caught: ', error);
-    res.send('An error has occurred :(');
+    if (res)
+    {
+        res.send('An error has occurred :(');
+    }
 }
 
 const app = express();
+
+app.use(cookieParser())
 
 //middleware for parsing http requests
 var bodyparser = require('body-parser');
@@ -38,6 +44,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 //package so people can request our apis without hell
 var cors = require('cors');
+const { query } = require('express');
 
 
 const server = new WebSocket.Server({
@@ -49,7 +56,23 @@ server.on('connection', function(socket) {
     sockets.push(socket);
 
     socket.on('message', function(msg) {
-        sockets.forEach(s => s.send(msg));
+        data = JSON.parse(msg);
+        console.log(data)
+
+        if (data.mtype = 'chat') {
+            let sql = `SELECT nickname, color FROM user WHERE name = '${data.user}'`;
+            db.query(sql, (err, result) => {
+                if (err) { catchException(err); return; }
+                
+                if (result[0] == undefined) { sockets.filter(s => s !== socket); return;}
+
+                let user = { 'name': result[0].nickname, 'color': result[0].color }
+                let message = data.message;
+                
+                let reply = JSON.stringify({ message, user });
+                sockets.forEach(s => s.send(reply));
+            });
+        }
     });
 
     socket.on('close', function() {
@@ -98,8 +121,91 @@ app.get('/index', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 });
 
-app.get('/chat/', (req, res) => {
-    res.sendFile(__dirname + "/chat.html");
+
+function validateCredentials(sql, user, pass)
+{
+    return new Promise((resolve, reject) => {
+        db.query(sql, [user], (err, result) => {
+            if (err) { catchException(err, req, res); reject(); return; }
+            
+            data = result[0];
+            if (data == undefined) { reject(); return;}
+
+            if (user.toLowerCase() == data.name.toLowerCase() && pass == data.password) resolve()
+            else reject();
+        })
+    })
+}
+
+app.get('/chat', (req, res) => {
+    console.log(req.cookies['user']);
+    if (!req.cookies['user']) { res.redirect("/chatlogin"); return; }
+    let iname = req.cookies['user']
+    let ipass = req.cookies['password']
+
+    validateCredentials(`SELECT name, password FROM user WHERE name=?`, iname, ipass).then(
+        function () {
+            res.cookie('user', iname, { maxAge: 900000 });//, httpOnly: true });
+            res.cookie('password', ipass, { maxAge: 900000 });//, httpOnly: true });
+            res.sendFile(__dirname + '/chat.html')
+            console.log('AAAAAAAAAA')
+        },
+        function () {
+            console.log('Invalid user...YEET!')
+            res.cookie('user', null, {maxAge: 0});
+            res.cookie('password', null, {maxAge: 0});
+            res.redirect("/chatlogin");
+        }
+    )
+});
+
+app.get('/chatlogin', (req, res) => {
+    if (req.cookies['user']) { res.redirect("/chat"); return; }
+    res.sendFile(__dirname + "/chatlogin.html")
+});
+
+app.post('/chatlogin', (req, res) => {
+    let iname = req.body["username"];
+    let ipass = req.body["password"];
+
+    validateCredentials(`SELECT name, password FROM user WHERE name = '${iname}'`, iname, ipass).then(
+        function () {
+            res.cookie('user', iname, { maxAge: 900000 });//, httpOnly: true });
+            res.cookie('password', ipass, { maxAge: 900000 });//, httpOnly: true });
+            res.redirect('/chat')
+        },
+        function () {
+            console.log('Invalid login attempt');
+            res.redirect('/chatlogin');
+        }
+    )
+});
+
+app.get('/chatlogout', (req, res) => {
+    res.cookie('user', null, {maxAge: 0});
+    res.cookie('password', null, {maxAge: 0});
+    res.redirect("/chatlogin");
+});
+
+app.post('/chatregister', (req, res) => {
+    let iname = req.body["username"];
+    let ipass = req.body["password"];
+    let icol = req.body["color"];
+
+    var numpatt = /^[0-9]+$/;
+        if (!numpatt.test(ipass))
+        {
+            res.send("Invalid password.");
+            return;
+        }
+
+    let sql = `INSERT INTO user (name, password, color, nickname) VALUES ('${iname}', ${ipass}, '${icol}', '${iname}');`;
+    db.query(sql, (err, result) => {
+        if (err) { catchException(err, req, res); return; }
+        res.cookie('user', iname, { maxAge: 900000 });
+        res.cookie('password', ipass, { maxAge: 900000 });
+        res.redirect('/chat')
+    });
 });
 
 app.get('/colorpage/', (req, res) => {
